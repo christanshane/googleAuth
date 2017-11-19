7160c30d3d0fd4c4ded059a1bba87231e07887b0
cosmoTechnologist <cosmoTechnologist@yahoo.com>
2017-11-19 20:30:02 +0800

    finished SignUpActivity, now registering to FirebaseAuth

--- app/src/main/java/com/example/cosmo/googleauth/MainActivity.java
+++ app/src/main/java/com/example/cosmo/googleauth/MainActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -13,21 +14,33 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
-public class MainActivity extends AppCompatActivity {
-    private Button signupRedirect;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    private Button signupRedirect, loginBtn;
+    EditText loginEmail, loginPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         signupRedirect = (Button)findViewById(R.id.signupRedirect);
+        loginBtn = (Button)findViewById(R.id.loginBtn);
+        loginEmail = (EditText)findViewById(R.id.loginEmail);
+        loginPassword = (EditText)findViewById(R.id.loginPassword);
 
-        signupRedirect.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+        signupRedirect.setOnClickListener(this);
+        loginBtn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch(view.getId()){
+            case R.id.loginBtn:
+
+                break;
+            case R.id.signupRedirect:
                 finish();
-                startActivity(new Intent(getApplicationContext(), SignUpActivity.class));
-            }
-        });
+                startActivity(new Intent(this, SignUpActivity.class));
+                break;
+        }
     }
 }
--- app/src/main/java/com/example/cosmo/googleauth/SignUpActivity.java
+++ app/src/main/java/com/example/cosmo/googleauth/SignUpActivity.java
@@ -1,13 +1,87 @@
 package com.example.cosmo.googleauth;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class SignUpActivity extends AppCompatActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class SignUpActivity extends AppCompatActivity implements View.OnClickListener {
+    private Button loginRedirect, signupBtn;
+    EditText signupEmail, signupPassword;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        loginRedirect = (Button)findViewById(R.id.loginRedirect);
+        signupBtn = (Button)findViewById(R.id.signupBtn);
+        signupEmail = (EditText)findViewById(R.id.signupEmail);
+        signupPassword = (EditText)findViewById(R.id.signupPassword);
+        mAuth = FirebaseAuth.getInstance();
+
+        loginRedirect.setOnClickListener(this);
+        signupBtn.setOnClickListener(this);
+    }
+
+    private void registerUser(){
+        String email = signupEmail.getText().toString();
+        String pass = signupPassword.getText().toString();
+
+        if(email.isEmpty()){
+            signupEmail.setError("Email is Required");
+            signupEmail.requestFocus();
+            return;
+        }
+
+        if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            signupEmail.setError("Please enter a valid email");
+            signupEmail.requestFocus();
+            return;
+        }
+
+        if(pass.isEmpty()){
+            signupPassword.setError("Password is Required");
+            signupPassword.requestFocus();
+            return;
+        }
+
+        if(pass.length()<6){
+            signupPassword.setError("Minimum length of password should be six(6) characters");
+            signupPassword.requestFocus();
+            return;
+        }
+
+        mAuth.createUserWithEmailAndPassword(email,pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    Toast.makeText(getApplicationContext(), "User Registered Successfully.", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch(view.getId()){
+            case R.id.signupBtn:
+                registerUser();
+                break;
+            case R.id.loginRedirect:
+                finish();
+                startActivity(new Intent(this, MainActivity.class));
+                break;
+        }
     }
 }
