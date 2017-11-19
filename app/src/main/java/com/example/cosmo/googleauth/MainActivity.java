c15ed8d34a4bb3fd90df83082ea6374a42e2a66b
cosmoTechnologist <cosmoTechnologist@yahoo.com>
2017-11-19 20:01:25 +0800

    added Dependencies

--- app/src/main/java/com/example/cosmo/googleauth/MainActivity.java
+++ app/src/main/java/com/example/cosmo/googleauth/MainActivity.java
@@ -1,13 +1,33 @@
 package com.example.cosmo.googleauth;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
+    private Button signupRedirect;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        signupRedirect = (Button)findViewById(R.id.signupRedirect);
+
+        signupRedirect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+                startActivity(new Intent(getApplicationContext(), SignUpActivity.class));
+            }
+        });
     }
 }
